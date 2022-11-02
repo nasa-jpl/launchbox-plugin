40d05e4f9d0a5592427a1a485aa5d23a49a3a35c
Luis Espinoza <Luis.A.Espinoza@jpl.nasa.gov>
2022-11-02 11:35:42 -0700

    updating distribution method
    setup.py being deprecated

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="launchbox-plugin",
-    description="A python helper class for integrating Launchbox plugins",
-    author="NASA-JPL",
-    author_email="dl-webdev@jpl.nasa.gov",
-    url="https://github.com/nasa-jpl/launchbox-plugin",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=["bottle==0.12.20"],
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
-)
+setup()
